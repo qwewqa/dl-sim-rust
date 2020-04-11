57b715e1916aa972b2baa99dec7dc87aa623d081
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2020-04-11 20:57:25 +0000

    timeline for single type

--- src/core/timeline.rs
+++ src/core/timeline.rs
@@ -3,91 +3,77 @@ use std::collections::BinaryHeap;
 use std::cell::{RefCell, Cell};
 use std::rc::{Rc, Weak};
 use std::any::Any;
+use bumpalo::Bump;
 
-type RcRefCellBox<T> = Rc<RefCell<Box<T>>>;
-
-pub fn new_rc_ref_cell_box<T>(x: T) -> RcRefCellBox<T> {
-    Rc::new(RefCell::new(Box::new(x)))
+pub struct Timeline<T> {
+    time: f64,
+    queue: BinaryHeap<Rc<Trigger<T>>>,
 }
 
-pub struct Timeline {
-    time: Cell<f64>,
-    running: Cell<bool>,
-    queue: RefCell<BinaryHeap<Rc<Trigger>>>,
-}
-
-impl Timeline {
-    pub fn new() -> Timeline {
+impl<T> Timeline<T> {
+    pub fn new() -> Timeline<T> {
         Timeline {
-            time: Cell::new(0.0),
-            running: Cell::new(false),
-            queue: RefCell::new(BinaryHeap::new()),
+            time: 0.0,
+            queue: BinaryHeap::new(),
         }
     }
 
-    pub fn schedule<F: FnMut() -> () + 'static>(&self, delay: f64, action: F) -> Weak<Trigger> {
+    pub fn schedule(&mut self, delay: f64, value: T) -> Rc<Trigger<T>> {
+        self.schedule_abs(self.time + delay, value)
+    }
+
+    pub fn schedule_abs(&mut self, time: f64, value: T) -> Rc<Trigger<T>> {
+        assert!(time >= self.time);
         let r = Rc::new(Trigger {
-            time: self.time.get() + delay,
-            action: RefCell::new(Box::new(action)),
+            time,
+            value,
             cancelled: Cell::new(false),
         });
-        self.queue.borrow_mut().push(r.clone());
-        Rc::downgrade(&r)
-    }
-
-    pub fn run(&self) {
-        if self.running.get() { panic!() }
-        self.running.set(true);
-        while self.running.get() {
-            let next = self.queue.borrow_mut().pop();
-            if next.is_none() { break; }
-            let next = next.unwrap();
-            if next.cancelled.get() { continue; };
-            self.time.set(next.time);
-            (&mut *next.action.borrow_mut())();
-        }
-    }
-
-    pub fn is_running(&self) -> bool {
-        self.running.get()
-    }
-
-    pub fn stop(&self) {
-        self.running.set(false);
-    }
-
-    pub fn now(&self) -> f64 {
-        self.time.get()
+        self.queue.push(r.clone());
+        r
     }
 }
 
-pub struct Trigger {
+impl<T> Iterator for Timeline<T> {
+    type Item = Rc<Trigger<T>>;
+
+    fn next(&mut self) -> Option<Rc<Trigger<T>>> {
+        while let Some(next) = self.queue.pop() {
+            if next.cancelled.get() { continue; }
+            self.time = next.time;
+            return Some(next)
+        }
+        None
+    }
+}
+
+pub struct Trigger<T> {
     time: f64,
-    action: RefCell<Box<dyn FnMut()>>,
+    value: T,
     cancelled: Cell<bool>,
 }
 
-impl Trigger {
-    fn cancel(&self) {
+impl<T> Trigger<T> {
+    pub fn cancel(&self) {
         self.cancelled.set(true);
     }
 }
 
-impl Eq for Trigger {}
+impl<T> Eq for Trigger<T> {}
 
-impl PartialEq for Trigger {
+impl<T> PartialEq for Trigger<T> {
     fn eq(&self, other: &Self) -> bool {
         self.time == other.time
     }
 }
 
-impl Ord for Trigger {
+impl<T> Ord for Trigger<T> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.partial_cmp(other).unwrap()
     }
 }
 
-impl PartialOrd for Trigger {
+impl<T> PartialOrd for Trigger<T> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         other.time.partial_cmp(&self.time)
     }
@@ -102,148 +88,70 @@ mod tests {
     use std::borrow::Borrow;
 
     #[test]
-    fn schedule() {
-        struct Context {
-            timeline: Timeline,
-            flag: Cell<bool>,
-        }
-        let c = Rc::new(Context {
-            timeline: Timeline::new(),
-            flag: Cell::new(false),
-        });
+    fn schedule_single() {
+        let mut tl = Timeline::new();
+        tl.schedule(10.0, ());
 
-        let tl = &c.timeline;
-        let r = Rc::downgrade(&c.clone());
-        let timing = 10.0;
-        tl.schedule(timing, move || {
-            let r = r.upgrade().unwrap();
-            assert_eq!(r.timeline.now(), timing);
-            r.flag.replace(true);
-        });
-        tl.run();
-        assert!(c.flag.get());
+        let n = tl.next().unwrap();
+        assert_eq!(n.time, 10.0);
+        assert_eq!(tl.time, 10.0);
+
+        assert!(tl.next().is_none());
+    }
+
+    #[should_panic]
+    #[test]
+    fn negative_delay() {
+        let mut tl = Timeline::new();
+        tl.schedule(-1.0, ());
+    }
+
+    #[should_panic]
+    #[test]
+    fn schedule_in_past() {
+        let mut tl = Timeline::new();
+        tl.schedule(10.0, ());
+        tl.next();
+        tl.schedule_abs(9.0, ());
+    }
+
+    #[test]
+    fn schedule_order() {
+        let mut tl = Timeline::new();
+        tl.schedule(3.0, 3.0);
+        tl.schedule(1.0, 1.0);
+        tl.schedule(2.0, 2.0);
+
+        let n = tl.next().unwrap();
+        assert_eq!(tl.time, 1.0);
+        assert_eq!(n.time, 1.0);
+        assert_eq!(n.value, 1.0);
+        let n = tl.next().unwrap();
+        assert_eq!(tl.time, 2.0);
+        assert_eq!(n.time, 2.0);
+        assert_eq!(n.value, 2.0);
+        let n = tl.next().unwrap();
+        assert_eq!(tl.time, 3.0);
+        assert_eq!(n.time, 3.0);
+        assert_eq!(n.value, 3.0);
+        assert!(tl.next().is_none())
     }
 
     #[test]
     fn cancellation() {
-        struct Context {
-            timeline: Timeline,
-            flag: Cell<bool>,
-        }
-        let c = Rc::new(Context {
-            timeline: Timeline::new(),
-            flag: Cell::new(false),
-        });
+        let mut tl = Timeline::new();
+        tl.schedule(3.0, 3.0);
+        tl.schedule(1.0, 1.0);
+        tl.schedule(2.0, 2.0).cancel();
 
-        let tl = &c.timeline;
-        let r = Rc::downgrade(&c.clone());
-        let timing = 10.0;
-        let trigger = tl.schedule(timing, move || {
-            r.upgrade().unwrap().flag.replace(true);
-        });
-        trigger.upgrade().unwrap().cancel();
-        tl.run();
-        assert!(!c.flag.get());
-    }
-
-    #[test]
-    fn ordering() {
-        struct Context {
-            timeline: Timeline,
-            counter: Cell<i32>,
-        }
-        let c = Rc::new(Context {
-            timeline: Timeline::new(),
-            counter: Cell::new(0),
-        });
-
-        let tl = &c.timeline;
-        let r = Rc::downgrade(&c.clone());
-        tl.schedule(3.0, move || {
-            let r = r.upgrade().unwrap();
-            assert_eq!(r.timeline.now(), 3.0);
-            assert_eq!(r.counter.get(), 2);
-            r.counter.replace(3);
-        });
-        let r = Rc::downgrade(&c.clone());
-        tl.schedule(1.0, move || {
-            let r = r.upgrade().unwrap();
-            assert_eq!(r.timeline.now(), 1.0);
-            assert_eq!(r.counter.get(), 0);
-            r.counter.replace(1);
-        });
-        let r = Rc::downgrade(&c.clone());
-        tl.schedule(2.0, move || {
-            let r = r.upgrade().unwrap();
-            assert_eq!(r.timeline.now(), 2.0);
-            assert_eq!(r.counter.get(), 1);
-            r.counter.replace(2);
-        });
-        tl.run();
-        assert_eq!(c.counter.get(), 3);
-    }
-
-    #[test]
-    fn stopping() {
-        struct Context {
-            timeline: Timeline,
-            counter: Cell<i32>,
-        }
-        let c = Rc::new(Context {
-            timeline: Timeline::new(),
-            counter: Cell::new(0),
-        });
-
-        let tl = &c.timeline;
-        let r = Rc::downgrade(&c.clone());
-        tl.schedule(1.0, move || {
-            let r = r.upgrade().unwrap();
-            assert_eq!(r.timeline.now(), 1.0);
-            assert_eq!(r.counter.get(), 0);
-            r.counter.replace(1);
-        });
-        let r = Rc::downgrade(&c.clone());
-        tl.schedule(2.0, move || {
-            let r = r.upgrade().unwrap();
-            assert_eq!(r.timeline.now(), 2.0);
-            assert_eq!(r.counter.get(), 1);
-            r.counter.replace(2);
-            r.timeline.stop();
-        });
-        tl.schedule(3.0, move || {
-            panic!()
-        });
-        tl.run();
-        assert_eq!(c.counter.get(), 2);
-    }
-
-    #[test]
-    fn nested_schedule() {
-        struct Context {
-            timeline: Timeline,
-            counter: Cell<i32>,
-        }
-        let c = Rc::new(Context {
-            timeline: Timeline::new(),
-            counter: Cell::new(0),
-        });
-
-        let tl = &c.timeline;
-        let r = Rc::downgrade(&c.clone());
-        tl.schedule(1.0, move || {
-            let r = r.upgrade().unwrap();
-            assert_eq!(r.timeline.now(), 1.0);
-            assert_eq!(r.counter.get(), 0);
-            r.counter.replace(1);
-            let ir = Rc::downgrade(&r.clone());
-            r.timeline.schedule(1.0, move || {
-                let ir = ir.upgrade().unwrap();
-                assert_eq!(ir.timeline.now(), 2.0);
-                assert_eq!(ir.counter.get(), 1);
-                ir.counter.replace(2);
-            });
-        });
-        tl.run();
-        assert_eq!(c.counter.get(), 2);
+        let n = tl.next().unwrap();
+        assert_eq!(tl.time, 1.0);
+        assert_eq!(n.time, 1.0);
+        assert_eq!(n.value, 1.0);
+        let n = tl.next().unwrap();
+        assert_eq!(tl.time, 3.0);
+        assert_eq!(n.time, 3.0);
+        assert_eq!(n.value, 3.0);
+        assert!(tl.next().is_none())
     }
 }
